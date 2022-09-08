0b1e08bb4ac4e6eb8844e72cfede8bb474e5800b
SamSnowdon <snowdonsam13@gmail.com>
2022-09-08 08:29:34 -0400

    Final Commit

--- src/main/java/com/employees/demo/EmployeeApplication.java
+++ src/main/java/com/employees/demo/EmployeeApplication.java
@@ -6,7 +6,19 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableWebMvc
+@EnableSwagger2
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EmployeeApplication {
 
@@ -14,6 +26,13 @@ public class EmployeeApplication {
 
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
+
+	@Bean
+	public RestTemplate getRestAPI(){
+		return new RestTemplateBuilder().build();
+	}
+
+
 
 
 
